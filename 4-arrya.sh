5bfd9cd9d288fe617e9706b431dd355d9144103a
kona93 <devopapavani8@gmail.com>
2023-12-18 18:16:01 +0530

    adding invarriable

---
+++ 4-arrya.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+NAMESOFTHEEMPLOYES=("kona" "puli" "dekka" "bomma" "namala" "karumuri" "bolledhu")
+
+echo "first value : $ {[0]}"
+echo "second value : $ {[1]}"
+echo "fifth value : $ {[2]}"
