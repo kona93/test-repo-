53a9819a1f4ef1d7a5318dcdb1bdf97e008eff63
kona93 <devopapavani8@gmail.com>
2023-12-18 18:27:17 +0530

    adding invarriable

--- 4-arrya.sh
+++ 4-arrya.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-EMPLOYES=("kona" "puli" "dekka" "bomma" "namala" "karumuri" "bolledhu")
+EMPLOYES=("kona "23" "puli "56" "dekka "43" "bomma "43" "namala "32" "karumuri "65" "bolledhu"45")
 
 echo "first value:: ${[0]}"
