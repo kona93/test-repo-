5af5d501d4d66a6ecafd1605b9ab8de95ba05756
kona93 <devopapavani8@gmail.com>
2023-12-18 17:57:32 +0530

    adding invarriable

--- 1-variable.sh
+++ 1-variable.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 DATE=$(date)
-PERSON1=Kona
-PERSON2=Gollu
+
+PERSON1=$1
+PERSON2=$2
 
 echo " HELLO World "
 echo " The date is now $DATE"
@@ -10,4 +11,4 @@ echo " The date is now $DATE"
 echo "PERSON1 ::hi .. $PERSON1 good morning"
 echo "PERSON2 :: HEllo .. $PERSON2 good morning "
 echo "PERSON1 :: how are you $PERSON2"
-echo "PERSON2 :: iam good , how about you $PERSON2"
+echo "PERSON2 :: iam good , how about you $PERSON1"
