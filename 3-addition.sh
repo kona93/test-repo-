134ac47f08bcaa6ac411bb74c4cd458fb1b0f97f
kona93 <devopapavani8@gmail.com>
2023-12-18 18:06:59 +0530

    adding invarriable

---
+++ 3-addition.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+NUMBER1=93837
+NUMBER2=98762
+
+sum=$($NUMBER1+$NUMBER2)
+echo "total :: $sum"
---
+++ shell.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#this is a hello world
+
+echo "HELLO PARENTS" #final exam results of the student
+echo $(date '+%Y-%m-%d')
+
+ NAME=KONA
+ telugu=90
+ hindi=90
+ english=78
+ maths=67
+ science=80
+ socail=90
+ totalsmarks=
+
+echo NAME of the student =$NAME
+echo naame of the subject=$telugu
+echo naame of the subject=$hindi
+echo naame of the subject=$english
+echo naame of the subject=$science
+echo naame of the subject=$socail
+echo total marks of the students=$t0talmarks
+
+sum=$(( $telugu + $hindi + $english + $english + $science + $socail))
+ echo total marks: $sum
